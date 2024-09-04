6cd906de0f9596b1e3af70f3914e996cb631de9f
Tulio <tuliocsaraiva@gmail.com>
2024-09-04 18:31:21 -0300

    Creating Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+    const date = new Date()
+
+  return (
+    <footer style={{paddingTop: "90vh"}}>
+        <Container>
+            <Row>
+                <Col className='text-center'>
+                    TulioShop &copy; {date.getFullYear()}
+                </Col>
+            </Row>
+        </Container>
+    </footer>
+  )
+}
+
+export default Footer
