ec95f5e2ee9d37b84d596f9cfa52f10f61331f6c
Tulio <tuliocsaraiva@gmail.com>
2024-09-06 10:36:45 -0300

    Fetching data from the backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 5000
+const products = require('./products')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/api/products', (req, res) => {
+    res.json(products)
 })
 
 app.listen(port, () => {
--- frontend/src/screens/homeScreen.jsx
+++ frontend/src/screens/homeScreen.jsx
@@ -1,8 +1,26 @@
 import { Row, Col } from 'react-bootstrap';
 import ProductCard from '../components/ProductCard';
-import products from '../products';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await axios.get('/api/products');
+        console.log(response)
+        setProducts(response.data)
+      } catch (err) {
+        setError(err.message);  // Lida com erros
+      } 
+    };
+
+    getProducts()
+  }, [])
+
   return (
     <>
       <h1>Latest Products</h1>
