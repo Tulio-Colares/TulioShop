107bf98d18174210d4c4516150750bfef9b60aa8
Tulio <tuliocsaraiva@gmail.com>
2024-09-13 14:14:02 -0300

    Cart items displayed in Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,11 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Badge, Container, Nav, Navbar } from 'react-bootstrap';
 import {LinkContainer} from 'react-router-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 
 function Header() {
+  const {cartItems} = useSelector((state) => state.cart)
+
   return (
     <Navbar expand="lg" bg="dark" data-bs-theme="dark">
       <Container>
@@ -17,6 +18,12 @@ function Header() {
             <LinkContainer to='/cart'>
               <Nav.Link>
                 <FaShoppingCart /> Cart
+
+                {cartItems.length > 0 && (
+                  <Badge pill bg='success' style={{marginLeft: '5px'}}>
+                    {cartItems.reduce((a, c) => a + c.qty, 0)}
+                  </Badge>
+                )}
               </Nav.Link>
             </LinkContainer>
             <LinkContainer to='/login'>
