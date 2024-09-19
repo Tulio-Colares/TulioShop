c1bcb0eaab5d45b37f3557fff7cc0e701568a155
Tulio <tuliocsaraiva@gmail.com>
2024-09-19 11:01:28 -0300

    Setting up private routes

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -7,16 +7,16 @@ import { saveShippingAddress } from '../slices/cartSlice';
 
 const ShippingScreen = () => {
   const cart = useSelector((state) => state.cart);
-  console.log(cart)
   const { shippingAddress } = cart;
+
   const [address, setAddress] = useState(shippingAddress?.address || '');
   const [city, setCity] = useState(shippingAddress?.city || '');
-  const [postalCode, setPostalCode] = useState(
-    shippingAddress?.postalCode || ''
-  );
+  const [postalCode, setPostalCode] = useState(shippingAddress?.postalCode || '');
   const [country, setCountry] = useState(shippingAddress?.country || '');
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
   const submitHandler = (e) => {
     e.preventDefault();
     dispatch(saveShippingAddress({ address, city, postalCode, country }));
@@ -27,6 +27,7 @@ const ShippingScreen = () => {
     <FormContainer>
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
+        
         <Form.Group className='my-2' controlId='address'>
           <Form.Label>Address</Form.Label>
           <Form.Control
@@ -37,6 +38,7 @@ const ShippingScreen = () => {
             onChange={(e) => setAddress(e.target.value)}
           ></Form.Control>
         </Form.Group>
+
         <Form.Group className='my-2' controlId='city'>
           <Form.Label>City</Form.Label>
           <Form.Control
@@ -47,6 +49,7 @@ const ShippingScreen = () => {
             onChange={(e) => setCity(e.target.value)}
           ></Form.Control>
         </Form.Group>
+
         <Form.Group className='my-2' controlId='postalCode'>
           <Form.Label>Postal Code</Form.Label>
           <Form.Control
@@ -57,6 +60,7 @@ const ShippingScreen = () => {
             onChange={(e) => setPostalCode(e.target.value)}
           ></Form.Control>
         </Form.Group>
+        
         <Form.Group className='my-2' controlId='country'>
           <Form.Label>Country</Form.Label>
           <Form.Control
@@ -67,9 +71,11 @@ const ShippingScreen = () => {
             onChange={(e) => setCountry(e.target.value)}
           ></Form.Control>
         </Form.Group>
+
         <Button type='submit' variant='primary'>
           Continue
         </Button>
+
       </Form>
     </FormContainer>
   );
